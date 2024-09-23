3138594e88092388ac40e90b44c02db6cd3f1bfc
Artur Khakimau <arthur78@gmail.com>
2024-09-23 09:18:47 +0200

    Update version and description in setup.py
    Change the package version from 0.0.0 to 0.1.0 to indicate the initial release. Also, update the description to clarify the functionality by specifying "Persistent queues" instead of "Implementing queues."

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="nque",
-    version="0.0.0",
-    description="Implementing queues",
+    version="0.1.0",
+    description="Persistent queues",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author="Artur Khakimau",
